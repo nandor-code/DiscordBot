df586f7374f9c287e3b982e3cd1ce91a31c85305
Szots <nandor@amazon.com>
2018-12-06 15:52:40 -0800

    structure cleanup

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 // include helpers
-include( 'bot_helpers.js' );
-include( 'commands.js' );
+const cmdList = require("../config/commands.json");
 
 // Load config file
 const config = require("../config/config.json");
@@ -114,3 +113,195 @@ client.on("message", async message => {
 
 // Run the bot
 client.login(config.token);
+
+// Command Helpers
+
+// Help Func
+help(args, message)
+{
+    if (args.length == 0)
+    {
+        generalHelp(message);
+    }
+    else
+    {
+        getHelp(args, message);
+    }
+}
+
+// Main Help Menu
+function generalHelp(message)
+{
+  let hArray = new Array();
+  for (var key in cmdList)
+  {
+    hArray.push(key);
+  }
+  message.author.send(topMenu + "Command List:\n\n " + hArray.toString().replace(/,/g, " ") + "\n\n" + botMenu);
+}
+
+// Help Sub-Menus
+function getHelp(args, message)
+{
+  try {
+    let arg1 = args[0];
+    let arg2 = args[1];
+    if (!isEmpty(arg1))
+    {
+      if (!isEmpty(cmdList[arg1]))
+      {
+        let example = cmdList[arg1]['example'];
+        let desc = cmdList[arg1]['desc'];
+        let cmdPerm = (message.member.permissions.has(cmdList[arg1]['perms']) ? "yes" : "no" );
+        if (arg1.toString().toLowerCase() === 'set' && isEmpty(arg2))
+        {
+          let optionsArray = new Array();
+          for(var key in cmdList['set']['options'])
+          {
+            optionsArray.push(key);
+          }
+          message.author.send(topMenu + "Command: " + arg1 + "\n\nSyntax: " + example + "\n\n" + "Description: " + desc + "\n\nOptions Available: " + optionsArray.toString().replace(/,/g, " ") + "\n\nFor more information on an option '**!help set <option>**'\n\nCan I use this? " + cmdPerm);
+          return;
+        }
+        if (arg1.toString().toLowerCase() === 'set' && !isEmpty(arg2) && !isEmpty(cmdList[arg1]['options'][arg2]))
+        {
+          example = cmdList[arg1]['options'][arg2]['example'];
+          desc = cmdList[arg1]['options'][arg2]['desc'];
+          cmdPerm = (message.member.permissions.has(cmdList[arg1]['options'][arg2]['perms']) ? "yes" : "no" );
+          message.author.send(topMenu + "Command: " + arg1 + " " + arg2 + "\n\nSyntax: " + example + "\n\n" + "Description: " + desc + "\n\nCan I use this? " + cmdPerm);
+          return;
+        }
+        else
+        {
+          message.author.send(topMenu + "Command: " + arg1 + "\n\nSyntax: " + example + "\n\n" + "Description: " + desc + "\n\nCan I use this? " + cmdPerm);
+          return;
+        }
+      }
+      else
+      {
+          message.author.send(`[${config.appname}] Error: No such command. For a list of commands type '**!help**' with no arguments in any channel.`);
+          return;
+      }
+    }
+  }
+  catch(error)
+  {
+    logIt(error.message, true);
+  }
+}
+
+// Generic Helpers
+function getUrl( hostName, pathToData, callBack )
+{
+    var data = '';
+
+    var request = http.request( { host: hostName, path: pathToData }, function (res) {
+        res.on('data', function (chunk) {
+            data += chunk;
+        });
+        res.on('end', function () {
+           callBack( data );
+        });
+    });
+
+    request.on('error', function (e) {
+        logIt(e.message);
+    });
+
+    request.end();
+}
+
+// Log certain items or errors
+function logIt(message, isError = false)
+{
+  if (!isError)
+  {
+    console.log(`[${config.appname}] ` + displayTime() + "> " + message);
+  }
+  else
+  {
+    console.error(`[${config.appname}] ` + displayTime() + "> " + message);
+  }
+}
+
+// Format Timestamps
+function displayTime() {
+    var str = "";
+    var currentTime = new Date()
+    var hours = currentTime.getHours()
+    var minutes = currentTime.getMinutes()
+    var seconds = currentTime.getSeconds()
+
+    if (minutes < 10)
+    {
+        minutes = "0" + minutes
+    }
+
+    if (seconds < 10)
+    {
+        seconds = "0" + seconds
+    }
+
+    str += hours + ":" + minutes + ":" + seconds + " ";
+
+    if(hours > 11)
+    {
+        str += "PM"
+    }
+    else
+    {
+        str += "AM"
+    }
+    return str;
+}
+
+// Remove item from array when callback is needed.
+function arrayRemove(arr, item)
+{
+    for (var i = arr.length; i--;)
+    {
+        if (arr[i] === item)
+        {
+            arr.splice(i, 1);
+            logIt("Removed " + item + " from " + arr + " array at index [" + i + "]");
+        }
+    }
+}
+
+// Is passed variable a number?
+function isNumeric(n)
+{
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+// Is passed variable or array empty
+function isEmpty(obj)
+{
+    for(var key in obj)
+    {
+        if(obj.hasOwnProperty(key))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Prototype Extensions
+// Does an array contain an item?
+Array.prototype.contains = function(obj)
+{
+    return this.indexOf(obj) > -1;
+};
+
+// Remove all instances of an item from an array
+Array.prototype.remove = function(item)
+{
+    for (var i = this.length; i--;)
+    {
+        if (this[i] === item)
+        {
+            this.splice(i, 1);
+        }
+    }
+}
